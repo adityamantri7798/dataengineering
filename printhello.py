8575ae5aaa64cc5549c2bd1e605c2ee37a8b9793
adityamantri7798 <adityamantri7798@gmail.com>
2024-07-07 01:53:40 -0700

    new_new

--- printhello.py
+++ printhello.py
@@ -1,4 +1,5 @@
 print("Hello world")
 a = 7
 b=85
+print("hii aditya mantri and shubham ")
 
--- shubham.py
+++ shubham.py
@@ -2,3 +2,4 @@ for num in range(1,50):
   for i in range(2,num):
     if num%i!=0:
       print(num)
+      print("hii hellow how are you")
